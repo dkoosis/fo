4cb4252d9f39c474b1b42d1c7d8c93f702a14154
dkoosis@gmail.com <dkoosis@gmail.com>
2025-12-15 13:37:19 -0500

    fix(dashboard): remove top padding from task list panel

--- pkg/dashboard/theme.go
+++ pkg/dashboard/theme.go
@@ -182,7 +182,8 @@ func (t *DashboardTheme) Compile() *CompiledTheme {
 	ct.TaskListStyle = lipgloss.NewStyle().
 		Border(lipgloss.RoundedBorder()).
 		BorderForeground(ct.colorBorder).
-		Padding(1, 2)
+		PaddingLeft(2).
+		PaddingRight(2)
 
 	ct.SelectedStyle = lipgloss.NewStyle().
 		Bold(true).
